de83482ec26047149877e1aef5cb4ffb65038925
SuperBFG7 <daniel@despite.ch>
2016-03-05 10:41:11 +0100

    ignore depencencies

--- system/pacmaster.sh
+++ system/pacmaster.sh
@@ -12,7 +12,8 @@ echo "checking AUR updates..."
 repo="custom"
 /usr/bin/pacman -Slp $repo | awk '{print $2}' | sort | \
 		diff --changed-group-format='%<' --unchanged-group-format='' --label "" - $PKGDEST/ignore.txt | while read i; do
-	local="`/usr/bin/pacman -Sp --print-format %v $repo/$i`"
+	local="`/usr/bin/pacman -Si $repo/$i | grep Version | sed -e "s/.* //"
+	`"
 	remote="`cower -s -b --format %v ^$i$`"
 
 	if [ "$local" = "$remote" ]; then
