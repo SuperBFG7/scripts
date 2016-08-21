1c5f92b343ff2207f2040917e4ab62f22b8218e0
SuperBFG7 <daniel@despite.ch>
2016-08-21 17:18:10 +0200

    add servers

--- backup/backup_pc.sh
+++ backup/backup_pc.sh
@@ -17,7 +17,14 @@ rsync $@ ${RSYNC_OPTS[@]} "/data/raid/" "$BKP/backup/raid/"
 
 header "/home/daniel/"
 rsync $@ ${RSYNC_OPTS[@]} "/home/daniel/" "$BKP/backup/home.daniel/"
+# backup pi data
+header "to backup pi data run:"
+echo rsync $@ -av --progress --delete --rsh=ssh --exclude mustang/ --exclude rsnapshot/ pimedia:/data/backup/ $BKP/backup/
 
 # backup root
-header "/etc/"
-echo run as root: rsync $@ ${RSYNC_OPTS[@]} "/etc/" "$BKP/backup/mustang/etc/"
+header "to backup /etc/ run as root:"
+echo rsync $@ ${RSYNC_OPTS[@]} "/etc/" "$BKP/backup/mustang/etc/"
+
+# backup pi root
+header "to backup pi root run as root:"
+echo rsync $@ -av --progress --delete --delete-excluded --rsh=ssh --include /rsnapshot/ --include /rsnapshot/daily.0/ --exclude rsnapshot/*/ pi3:/data/backup/ $BKP/backup/
