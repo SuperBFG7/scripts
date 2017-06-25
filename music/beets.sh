9d8844391e54250194018c895a002b46182cc9b4
SuperBFG7 <daniel@despite.ch>
2017-06-25 14:12:08 +0200

    use relative timestamps for beet

--- music/beets.sh
+++ music/beets.sh
@@ -5,17 +5,16 @@
 echo "beet list source::^$"
 echo
 
-today="`date +%Y-%m-%d`"
 echo "----- today"
-echo "beet modify added:\"${today}\"" source=$1
-echo "beet modify added:\"${today}\"" source=$1 manual=1
-echo "beet modify added:\"${today}\"" source=$1 new=1
-echo "beet modify added:\"${today}\"" source=$1 new=1 manual=1
-echo "beet modify added:\"${today}\"" source=$1 format:flac
-echo "beet modify added:\"${today}\"" source=$1 format:ogg
-echo "beet modify added:\"${today}\"" source=$1 format:mp3
-echo "beet modify added:\"${today}\"" source=$1 artist:$2
+echo "beet modify added:-1d.. source=$1"
+echo "beet modify added:-1d.. source=$1 manual=1"
+echo "beet modify added:-1d.. source=$1 new=1"
+echo "beet modify added:-1d.. source=$1 new=1 manual=1"
+echo "beet modify added:-1d.. source=$1 format:flac"
+echo "beet modify added:-1d.. source=$1 format:ogg"
+echo "beet modify added:-1d.. source=$1 format:mp3"
+echo "beet modify added:-1d.. source=$1 artist:$2"
 echo
 echo "----- write & move"
-echo "beet write added:\"${today}\"; beet move added:\"${today}\""
+echo "beet write added:-1d..; beet move added:-1d.."
 echo
