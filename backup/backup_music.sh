8d0af554633dbe935b041d72e00cc5ed9df9503c
SuperBFG7 <daniel@despite.ch>
2017-08-01 13:14:49 +0200

    fixed excludes

--- backup/backup_music.sh
+++ backup/backup_music.sh
@@ -4,7 +4,7 @@
 
 setup_backup
 
-EXCLUDE="library|new|shared"
+EXCLUDE="library|new|shared|rip"
 for i in "$ORIG/music/"*/; do
 	backup_exclude $EXCLUDE $i $@
 done
@@ -13,4 +13,4 @@ for i in "$ORIG/music/_new/"*/; do
 	backup_exclude "zz_" $i $@
 done
 
-backup "music/library/" $@ #--exclude "high/" --exclude "new/"
+backup "music/library/" $@ --exclude "high/" --exclude "new/"
