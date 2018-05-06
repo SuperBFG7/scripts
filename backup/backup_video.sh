aa19e65a932ab1abecf3c0b58561342d029079bc
SuperBFG7 <daniel@despite.ch>
2018-05-06 12:39:35 +0200

    simplified

--- backup/backup_video.sh
+++ backup/backup_video.sh
@@ -5,10 +5,5 @@
 setup_backup
 
 backup "video/" $@ \
-	--exclude 'movies.incoming/' \
-	--exclude 'queue/' \
-	--exclude 'sickrage/' \
-	--exclude 'sickrage.movies/' \
-	--exclude 'tv/'
-
-backup2 "video/new/sickrage/movies/" "video/sickrage.movies/" $@
+	--exclude 'tv/' \
+	--exclude 'tv.archiv/'
