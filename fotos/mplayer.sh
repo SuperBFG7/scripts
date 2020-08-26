3b696d4d466504c4ca1f895d3f7977f83d21eb02
SuperBFG7 <daniel@despite.ch>
2020-08-26 16:25:57 +0200

    added autorotate wrapper for mplayer

---
+++ fotos/mplayer.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+#
+# Automatically rotate video in mplayer based on EXIF tags
+# based on https://ygrek.org/p/code/mplayer_autorotate
+# (added support for multiple files given add once)
+
+args=()
+
+while (( "$#" )); do
+	f="$1"
+	shift
+	if [ ! -f "$f" ]; then
+		args+=( "$f" )
+		continue
+	fi
+
+	case $(exiftool -rotation -b "$f") in
+		90) rot="-vf rotate=1" ;;
+		180) rot="-vf flip,mirror" ;;
+		270) rot="-vf rotate=2" ;;
+		*) rot="" ;;
+	esac
+
+	mplayer ${args[@]} $rot "$f"
+done
+
