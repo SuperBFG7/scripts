79718d3ed7234a78745386d7c1108cbf7895ae3b
SuperBFG7 <daniel@despite.ch>
2022-07-28 15:07:39 +0200

    shellcheck and switch from cower to aur

--- system/pacmaster.sh
+++ system/pacmaster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. `dirname "$0"`/includes.sh
+. "$(dirname "$0")/includes.sh"
 
 if hash pacmatic 2>/dev/null; then
-	pacmatic $@
+	pacmatic "$@"
 else
 	echo "*** WARNING: pacmatic not found, using pacman instead ***"
-	/usr/bin/pacman $@
+	/usr/bin/pacman "$@"
 fi
 
 if [ "$1" != "-Syu" ]; then
@@ -18,23 +18,13 @@ if hash pkgfile 2>/dev/null; then
 else
 	echo "*** WARNING: pkgfile not found, skipping ***"
 fi
-if ! hash cower 2>/dev/null; then
-	echo "*** WARNING: cower not found, skipping AUR updates ***"
+
+systemctl daemon-reload
+
+if ! hash aur 2>/dev/null; then
+	echo "*** WARNING: aurutils not found, skipping AUR updates ***"
 	exit 0
 fi
-systemctl daemon-reload
 header "checking AUR updates..."
-. /etc/makepkg.conf
-repo="custom"
-/usr/bin/pacman -Slp $repo | awk '{print $2}' | sort | \
-		diff --changed-group-format='%<' --unchanged-group-format='' --label "" - $PKGDEST/ignore.txt | while read i; do
-	local="`/usr/bin/pacman -Si $repo/$i | grep Version | sed -e "s/.* //"
-	`"
-	remote="`cower -s --format %v ^$i$`"
-
-	if [ "$local" = "$remote" ]; then
-		echo "$i is up to date"
-	else
-		echo "$i: $local --> $remote"
-	fi
-done
+aur repo --upgrades
+aur repo -l | aur vercmp -a -u 2 | grep "<-" | sed -e "s/-$/is missing in AUR/"
