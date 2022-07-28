094778f1995c1e451e010350649aab56961125d6
SuperBFG7 <daniel@despite.ch>
2022-07-28 15:26:11 +0200

    fix expansion

--- system/cleanup.sh
+++ system/cleanup.sh
@@ -3,7 +3,7 @@
 . "$(dirname "$0")/includes.sh"
 
 header "removing unused orphan packages"
-eval pacman -Rns "$(/usr/bin/pacman -Qtdq)"
+pacman -Rns "$(/usr/bin/pacman -Qtdq)"
 
 header "cleaning package cache: old versions"
 paccache -r
