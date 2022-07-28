d6cd10e34419324ede904739cbff2092de97b69b
SuperBFG7 <daniel@despite.ch>
2022-07-28 15:31:16 +0200

    fix word splitting

--- system/cleanup.sh
+++ system/cleanup.sh
@@ -3,7 +3,8 @@
 . "$(dirname "$0")/includes.sh"
 
 header "removing unused orphan packages"
-pacman -Rns "$(/usr/bin/pacman -Qtdq)"
+# shellcheck disable=SC2046
+pacman -Rns $(/usr/bin/pacman -Qtdq)
 
 header "cleaning package cache: old versions"
 paccache -r
