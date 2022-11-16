3f3a7610fe47f727ff7bc0e3ba78185ff0fc9199
SuperBFG7 <daniel@despite.ch>
2022-11-16 19:27:54 +0100

    handle empty video timestamps

--- fotos/vrename.sh
+++ fotos/vrename.sh
@@ -19,8 +19,12 @@ for i in *.{mp4,mov}; do
 	if [ -z "$timestamp" ]; then
 		timestamp=`exiftool "$i" | grep -i "Media Create Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g"`
 	fi
+	if [ "$timestamp" = "0000-00-00_000000" ]; then
+		echo "$i: timestamp empty, using file modification date - PLEASE CHECK"
+		timestamp=`exiftool "$i" | grep -i "File Modification Date/Time" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g"`
+	fi
 	if [ -z "$timestamp" ]; then
-		echo "$i has no timestamp"
+		echo "$i: no timestamp found, skipping"
 		continue
 	fi
 	$cmd "$i" "$timestamp.${i##*.}"
