f8351787238a361dc87b02a0860d60cf5f7d0029
SuperBFG7 <daniel@despite.ch>
2022-12-28 20:58:29 +0100

    more control over renames

--- fotos/vrename.sh
+++ fotos/vrename.sh
@@ -2,31 +2,52 @@
 
 . `dirname "$0"`/includes.sh
 
-cmd="echo mv -i"
+cmd="echo mv -iv"
+level="$1"
+action="$2"
 
-if [ -z "$1" ]; then
-	cmd="mv -i"
+if [ -z "$level" ]; then
+	usage "level [action]"
+	echo "levels:"
+	echo " safe    = only consider files with exif timestamps"
+	echo " unsafe1 = only consider files missing exif timestamp and use modification date instead"
+	echo
+	echo "actions:"
+	echo " dry = do nothing, display suggested renames (default)"
+	echo " wet = perform the renames"
+	exit 0
+fi
+
+if [ "$action" = "dry" ]; then
+	cmd="mv -iv"
 fi
 
 for i in *.{mp4,mov}; do
 	if [ ! -f "$i" ]; then
 		continue
 	fi
-	timestamp=`exiftool "$i" | grep -i "Date/Time Original" | head -1 | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g"`
+	type="safe"
+	timestamp=`exiftool "$i" | grep -i "Date/Time Original" | head -1 | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	if [ -z "$timestamp" ]; then
-		timestamp=`exiftool "$i" | grep -i "Creation Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g"`
+		timestamp=`exiftool "$i" | grep -i "Creation Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	fi
 	if [ -z "$timestamp" ]; then
-		timestamp=`exiftool "$i" | grep -i "Media Create Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g"`
+		timestamp=`exiftool "$i" | grep -i "Media Create Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	fi
 	if [ "$timestamp" = "0000-00-00_000000" ]; then
-		echo "$i: timestamp empty, using file modification date - PLEASE CHECK"
-		timestamp=`exiftool "$i" | grep -i "File Modification Date/Time" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g"`
+		type="unsafe"
+		timestamp=`exiftool "$i" | grep -i "File Modification Date/Time" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	fi
 	if [ -z "$timestamp" ]; then
-		echo "$i: no timestamp found, skipping"
+		echo "# $i: no timestamp found, skipping"
 		continue
 	fi
+
+	if [ "$level" != "$type" ]; then
+		echo "# skipping $i - wrong level"
+		continue
+	fi
+
 	$cmd "$i" "$timestamp.${i##*.}"
 done
 
