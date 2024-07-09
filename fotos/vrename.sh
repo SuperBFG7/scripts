77f8e87f6abdf6cb3db6baad5d8b4ddcf728cdf4
SuperBFG7 <daniel@despite.ch>
2024-07-09 20:26:44 +0200

    option to update EXIF timestamp

--- fotos/vrename.sh
+++ fotos/vrename.sh
@@ -21,8 +21,9 @@ case $level in
 		echo " all    = consider all files, safe and unsafe (default)"
 		echo
 		echo "actions:"
-		echo " dry = do nothing, display suggested renames (default)"
-		echo " wet = perform the renames"
+		echo " dry  = do nothing, display suggested renames (default)"
+		echo " wet  = perform the renames"
+		echo " exif = set the EXIF CreateDate based on the filename"
 		exit 0
 		;;
 esac
@@ -32,21 +33,28 @@ if [ "$action" = "wet" ]; then
 	hint="true"
 fi
 
-for i in *.{mp4,mov}; do
+for i in *.{mp4,mov,avi}; do
 	if [ ! -f "$i" ]; then
 		continue
 	fi
 	type="safe"
-	timestamp=`exiftool "$i" | grep -i "Date/Time Original" | head -1 | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
+	timestamp=`exiftool "$i" | grep -i "^Date/Time Original" | head -1 | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	if [ -z "$timestamp" ]; then
-		timestamp=`exiftool "$i" | grep -i "Creation Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
+		timestamp=`exiftool "$i" | grep -i "^Creation Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	fi
 	if [ -z "$timestamp" ]; then
-		timestamp=`exiftool "$i" | grep -i "Media Create Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
+		timestamp=`exiftool "$i" | grep -i "^Create Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
+	fi
+	if [ -z "$timestamp" ]; then
+		timestamp=`exiftool "$i" | grep -i "^Media Create Date" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
 	fi
 	if [ "$timestamp" = "0000-00-00_000000" ]; then
 		type="unsafe"
 		timestamp=`exiftool "$i" | grep -i "File Modification Date/Time" | sed -e "s/.* : //" -e "s/[Z+].*//" -e "s/:/-/" -e "s/:/-/" -e "s/ /_/" -e "s/://g" | head -1`
+	fi
+	if [ -z "$timestamp" ]; then
+		type="unsafe"
+		timestamp=`date -r "$i" +%Y-%m-%d_%H%M%S`
 	fi
 	if [ -z "$timestamp" ]; then
 		echo "# $i: no timestamp found, skipping"
@@ -56,6 +64,11 @@ for i in *.{mp4,mov}; do
 	if [ "$level" != "all" -a "$level" != "$type" ]; then
 		echo "# skipping $i - wrong level"
 		continue
+	fi
+
+	if [ "$action" = "exif" ]; then
+		echo "setting EXIF CreateDate based on filename for ${i}"
+		exiftool -api QuickTimeUTC "-CreateDate<Filename" "${i}"
 	fi
 
 	$cmd "$i" "$timestamp.${i##*.}"
