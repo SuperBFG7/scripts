f875741afc2974023b857551bd54fa6aa2b70ed7
SuperBFG7 <daniel@despite.ch>
2024-10-21 10:53:00 +0200

    additional snippets added

--- system/misc.sh
+++ system/misc.sh
@@ -15,3 +15,12 @@ apulse /usr/bin/firefox
 sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
 # turn on webcam
 gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
+
+# compress pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf input.pdf
+
+# cut out part of a movie
+ffmpeg -ss 00:25 -i 2022-10-09_154900_Happy_Birthday_r1.mov -c copy foo.mov
+
+# find broken symlinks
+find . -xtype l
